da20ed2eea7e2ef23242239fa4f36dc033aaffd9
Vladimir <54625163+Vladimir-xx@users.noreply.github.com>
2021-05-05 20:51:48 +0300

    Создали 4- компонента и прописали на них роуты.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './shared/main-layout/main-layout.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {CartPageComponent} from './cart-page/cart-page.component';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: MainPageComponent},
+      {path: 'product/:id', component: ProductPageComponent},
+      {path: 'cart', component: CartPageComponent}
+    ]
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
